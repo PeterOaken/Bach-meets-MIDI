1379608cd0eb6ad34326265e502b6045085edca1
Shahin-A <shahina@berkeley.edu>
2014-05-08 23:38:13 -0700

    song only learnt from twinkle twinkle little star

--- src/TheOdds/Player.java
+++ src/TheOdds/Player.java
@@ -5,6 +5,12 @@ public class Player {
 	
 	public static void main(String[] argv){
 		FileNameResolver fnr = new FileNameResolver("data");
+		String path = "";
+		boolean toWrite = false;
+		if(argv.length==2){
+			path = argv[1];
+			toWrite = true;
+		}
 		
 		if (argv[0].equals("-complex")){
 			ComplexAnalyzer complexAnalyzer = new ComplexAnalyzer(fnr.getFiles());
@@ -12,18 +18,27 @@ public class Player {
 			
 			ComplexComposer complexComposer = new ComplexComposer();
 			complexComposer.play(0, complexAnalyzer);
+			if(toWrite){
+				complexComposer.write(path);
+			}
 		}else if(argv[0].equals("-simple")){
 			SimpleAnalyzer analyzer = new SimpleAnalyzer(fnr.getFiles());
 			analyzer.analyze();
 			
 			SimpleComposer simpleComposer = new SimpleComposer();
 			simpleComposer.play(0, analyzer);
+			if(toWrite){
+				simpleComposer.write(path);
+			}
 		}else if(argv[0].equals("-random")){		
 			RandomAnalyzer randomAnalyzer = new RandomAnalyzer();
 			randomAnalyzer.analyze();
 	
 			RandomComposer randomComposer = new RandomComposer();
 			randomComposer.play(0, randomAnalyzer);
+			if(toWrite){
+				randomComposer.write(path);
+			}
 		}else{
 			System.out.println("Please specify the input correctly");
 		}
